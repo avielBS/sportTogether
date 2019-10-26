fe8b2c6b3bd7f69d8141f3812a483be2da581061
avielBS <aviel2845@gmail.com>
2019-10-26 16:11:52 +0300

    add navigation drawer

--- app/src/main/java/com/example/sporttogether/MainActivity.java
+++ app/src/main/java/com/example/sporttogether/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sporttogether;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.sporttogether.Data.UserRecord;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -28,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private Button historyWorkoutsButton;
     private Button logoutButton;
 
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
     private FirebaseDatabase firebaseDatabase;
 
     private FirebaseAuth firebaseAuth;
@@ -38,54 +44,106 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginBtn = findViewById(R.id.login_btn);
-        workoutsButton = findViewById(R.id.workouts_btn);
-        historyWorkoutsButton = findViewById(R.id.workout_history_btn);
-        logoutButton = findViewById(R.id.logout_btn);
-
-        historyWorkoutsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent historyIntent = new Intent(MainActivity.this, HistoryActivity.class);
-                historyIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(historyIntent);
-            }
-        });
-
-
-        workoutsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent workoutsIntent = new Intent(MainActivity.this, WorkoutsActivity.class);
-                workoutsIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(workoutsIntent);
-            }
-        });
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-                loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(loginIntent);
-            }
-        });
-
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-            }
-        });
+//        loginBtn = findViewById(R.id.login_btn);
+//        workoutsButton = findViewById(R.id.workouts_btn);
+//        historyWorkoutsButton = findViewById(R.id.workout_history_btn);
+//        logoutButton = findViewById(R.id.logout_btn);
+//
+//        historyWorkoutsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent historyIntent = new Intent(MainActivity.this, HistoryActivity.class);
+//                historyIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(historyIntent);
+//            }
+//        });
+//
+//
+//        workoutsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent workoutsIntent = new Intent(MainActivity.this, WorkoutsActivity.class);
+//                workoutsIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(workoutsIntent);
+//            }
+//        });
+//
+//        loginBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+//                loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(loginIntent);
+//            }
+//        });
+//
+//        logoutButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+//            }
+//        });
 
 //        firebaseDatabase = FirebaseDatabase.getInstance();
 //        DatabaseReference dbRef = firebaseDatabase.getReference();
+
+        drawerLayout = findViewById(R.id.navigation_drawer);
+        navigationView = findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawe_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()){
+
+                    case R.id.add_workout_menu:
+                        Intent addWorkoutIntent = new Intent(MainActivity.this, AddWorkoutActiviy.class);
+                        addWorkoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(addWorkoutIntent);
+                        break;
+
+                    case R.id.search_workouts_menu:
+                        Intent workoutsIntent = new Intent(MainActivity.this, WorkoutsActivity.class);
+                        workoutsIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(workoutsIntent);
+                        break;
+                    case R.id.history_menu:
+                        Intent historyIntent = new Intent(MainActivity.this, HistoryActivity.class);
+                        historyIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(historyIntent);
+                        break;
+                    case R.id.settings:
+
+                        break;
+
+                    case R.id.login:
+                        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(loginIntent);
+                        break;
+                    case R.id.logout:
+                        FirebaseAuth.getInstance().signOut();
+                        break;
+
+
+                }
+
+                return true;
+            }
+
+        });
 
         firebaseAuth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(firebaseAuth.getCurrentUser() == null){
+                if (firebaseAuth.getCurrentUser() == null) {
                     Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
                     loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(loginIntent);
@@ -100,26 +158,29 @@ public class MainActivity extends AppCompatActivity {
         firebaseAuth.addAuthStateListener(authStateListener);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.drawer_menu,menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        if(item.getItemId() == R.id.login){
+//            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+//            loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            startActivity(loginIntent);
+//        }
+//        if(item.getItemId() == R.id.add_workout){
+//            Intent addWorkoutIntent = new Intent(MainActivity.this, AddWorkoutActiviy.class);
+//            addWorkoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            startActivity(addWorkoutIntent);
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if(item.getItemId() == R.id.login){
-            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-            loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(loginIntent);
-        }
-        if(item.getItemId() == R.id.add_workout){
-            Intent addWorkoutIntent = new Intent(MainActivity.this, AddWorkoutActiviy.class);
-            addWorkoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(addWorkoutIntent);
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
